e51ba4602267e49f0e1edd9150ae43651a54d829
familyProject <963600060@qq.com>
2018-12-28 10:39:20 +0800

    结构修改

--- src/main/java/com/zhoubi/graindepot/mapper/UserMapper.java
+++ src/main/java/com/zhoubi/graindepot/mapper/UserMapper.java
@@ -8,5 +8,5 @@ import tk.mybatis.mapper.common.Mapper;
  * Created by Administrator on 2018-12-5.
  */
 public interface UserMapper extends Mapper<User> {
-    public User getUserById(@Param("id") Integer id);
+    User getUserById(@Param("id") Integer id);
 }
