6f2be5def1efb8720d8aa3fbfb9ab746bfe6f9d0
fredma1987 <356803326@qq.com>
2019-02-19 20:35:35 +0800

    调整-fred

--- src/main/java/com/zhoubi/graindepot/UserApplication.java
+++ src/main/java/com/zhoubi/graindepot/UserApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.session.data.redis.RedisFlushMode;
@@ -12,7 +13,7 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
  * Created by Administrator on 2018-12-5.
  */
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableFeignClients
 @MapperScan(value = "com.zhoubi.graindepot.mapper")
 @EnableRedisHttpSession(redisFlushMode = RedisFlushMode.IMMEDIATE)
